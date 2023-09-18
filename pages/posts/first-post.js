94b75ad2b318175690144b9e81db33b9a551a974
nishioka-shinji <s61nov11.shinji.nishioka@gmail.com>
2023-09-18 12:05:05 +0900

    ページ間移動

--- pages/posts/first-post.js
+++ pages/posts/first-post.js
@@ -1,3 +1,13 @@
+import Link from 'next/link';
+
 export default function FirstPost() {
-  return <h1>First Post</h1>;
+  return (
+    <>
+      <h1>First Post</h1>
+      <h2>
+        <a href="/">Back to home A</a>
+        <Link href="/">Back to home</Link>
+      </h2>
+    </>
+  );
 }
