388af7b2d886a0523a6c3c537405393bfeecdc9d
nishioka-shinji <s61nov11.shinji.nishioka@gmail.com>
2023-09-18 12:15:41 +0900

    画像

--- pages/posts/first-post.js
+++ pages/posts/first-post.js
@@ -1,4 +1,14 @@
 import Link from 'next/link';
+import Image from 'next/image';
+
+const YourComponent = () => (
+  <Image
+    src="/images/profile.jpg" // Route of the image file
+    height={144} // Desired size with correct aspect ratio
+    width={144} // Desired size with correct aspect ratio
+    alt="Your Name"
+  />
+);
 
 export default function FirstPost() {
   return (
@@ -8,6 +18,8 @@ export default function FirstPost() {
         <a href="/">Back to home A</a>
         <Link href="/">Back to home</Link>
       </h2>
+      <img src="/images/profile.jpg" alt="Your Name" />
+      <YourComponent />
     </>
   );
 }
